577223e247c0638dc3050f9d413a262085937b67
Eii Chee Hieng <hieng2014@gmail.com>
2023-03-12 01:02:52 +0800

    Organized functionality into class

--- src/main.js
+++ src/main.js
@@ -1,37 +1,26 @@
 "use strict";
 
-class minimalGallery{
+export default class minimalGallery{
     constructor(){
-        const galleryContainer = document.createElement("div");
+        this.galleryContainer = document.createElement("div");   
 
-        const buttonContainer = document.createElement("div");
-        const scrollContainer = document.createElement("div");
-        
-        const prevButton = document.createElement("button");
-        const nextButton = document.createElement("button");
+        this.galleryContainer.classList.add("gallery-container");
 
-        const prevButtonIcon = document.createElement("span");
-        const nextButtonIcon = document.createElement("span");
+        this.scrollContainer = new scrollContainer().getElement();
 
-        galleryContainer.classList.add("gallery-container");
-        
-        buttonContainer.classList.add("button-container");
-        scrollContainer.classList.add("scroll-container");
+        this.galleryContainer.appendChild(this.scrollContainer);
+    }
 
-        prevButton.classList.add(["floating-button","prev-button"]);
-        nextButton.classList.add(["floating-button","next-button"]);
-
-        prevButtonIcon.classList.add("material-symbols-outlined");
-        nextButtonIcon.classList.add("material-symbols-outlined");
-        prevButtonIcon.textContent = "arrow_back_ios";
-        nextButtonIcon.textContent = "arrow_forward_ios";
-
-
+    getElement(){
+        return this.galleryContainer;
     }
 
     addGalleryItem(){
         const galleryItem = document.createElement("div");
+
+        galleryItem.classList.add("gallery-item");
         
+        this.scrollContainer.appendChild(galleryItem);
     }
     
     addImage(){
@@ -40,5 +29,65 @@ class minimalGallery{
 
     addVideo(){
 
+    }
+};
+
+class scrollContainer{
+    constructor(){
+        this.elem = document.createElement("div");
+
+        const prevButton = document.createElement("button");
+        const nextButton = document.createElement("button");
+
+        const prevButtonIcon = document.createElement("span");
+        const nextButtonIcon = document.createElement("span");
+
+        const buttonContainer = document.createElement("div");
+
+                
+        buttonContainer.classList.add("button-container");
+        this.elem.classList.add("scroll-container");
+
+        prevButton.classList.add("floating-button","prev-button");
+        nextButton.classList.add("floating-button","next-button");
+
+        prevButtonIcon.classList.add("material-symbols-outlined");
+        nextButtonIcon.classList.add("material-symbols-outlined");
+        prevButtonIcon.textContent = "arrow_back_ios";
+        nextButtonIcon.textContent = "arrow_forward_ios";
+
+
+        this.elem.appendChild(buttonContainer);
+
+        buttonContainer.appendChild(prevButton);
+        buttonContainer.appendChild(nextButton);
+
+        prevButton.appendChild(prevButtonIcon);
+        nextButton.appendChild(nextButtonIcon);
+
+        prevButton.addEventListener(
+            "click",
+            ()=>{
+                this.prev();
+            }
+        );
+        nextButton.addEventListener(
+            "click",
+            ()=>{
+                this.next();
+            }
+        );
+    }
+
+    getElement(){
+        return this.elem;
+    }
+
+    prev(){
+        this.getElement().scrollBy(-this.getElement().clientWidth,0);
+    }
+
+    next(){
+        this.getElement().scrollBy(this.getElement().clientWidth,0);
     }
 };
